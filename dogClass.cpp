0a6cf8d63163403569e2f291028932eaaf950b4f
c1arityy <talhaopus@gmail.com>
2026-05-01 13:10:15 +0500

    Added Dog and Dragon Classes + Made Separate files so everything is a bit more trackable

---
+++ dogClass.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "petClass.cpp"
+using namespace std;
+
+
+class Dog : public Pet {
+public:
+    Dog(string name);
+
+    void makeSound() override;
+    void draw() override;
+    string getType() override;
+
+    void bark();
+    void fetch();
+
+private:
+    float loyalty;
+};
+
+// Dog constructor
+Dog::Dog(string n) : Pet(n) {
+    loyalty = 90;
+}
+
+void Dog::makeSound() {
+    cout << name << " says: Woof! Woof!" << endl;
+}
+
+void Dog::draw() {
+    cout << "  / ^..^ \\" << endl;
+    cout << " ( o    o )" << endl;
+    cout << "  \\  --  / " << endl;
+    cout << "  " << name << " the Dog" << endl;
+}
+
+string Dog::getType() {
+    return "Dog";
+}
+
+void Dog::bark() {
+    if (!alive) {
+        cout << name << " is dead. Cannot bark." << endl;
+        return;
+    }
+    cout << name << " barks loudly: WOOF WOOF!" << endl;
+    energy -= 5;
+    happiness += 8;
+    if (happiness > 100) happiness = 100;
+    if (energy < 0) energy = 0;
+}
+
+void Dog::fetch() {
+    if (!alive) {
+        cout << name << " is dead. Cannot fetch." << endl;
+        return;
+    }
+
+    if (energy < 20) {
+        cout << name << " is too tired to fetch." << endl;
+        return;
+    }
+
+    cout << name << " fetched the ball!" << endl;
+    energy -= 20;
+    hunger += 10;
+    happiness += 15;
+    loyalty += 5;
+
+    if (happiness > 100) happiness = 100;
+    if (hunger > 100) hunger = 100;
+    if (loyalty > 100) loyalty = 100;
+}
---
+++ dragonClass.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "petClass.cpp"
+using namespace std;
+
+class Dragon : public Pet {
+public:
+    Dragon(string name);
+
+    void makeSound() override;
+    void draw() override;
+    string getType() override;
+
+    void breatheFire();
+
+private:
+    float fireLevel;
+};
+
+// Dragon constructor
+Dragon::Dragon(string n) : Pet(n) {
+    fireLevel = 60;
+}
+
+void Dragon::makeSound() {
+    cout << name << " says: ROAAARRR!" << endl;
+}
+
+void Dragon::draw() {
+    cout << "  >>==>" << endl;
+    cout << " ( o  o)" << endl;
+    cout << "  ) /\\ ( " << endl;
+    cout << "  " << name << " the Dragon" << endl;
+}
+
+string Dragon::getType() {
+    return "Dragon";
+}
+
+void Dragon::breatheFire() {
+    if (!alive) {
+        cout << name << " is dead. Cannot breathe fire." << endl;
+        return;
+    }
+
+    if (fireLevel < 20) {
+        cout << name << " doesn't have enough fire!" << endl;
+        return;
+    }
+
+    if (energy < 15) {
+        cout << name << " is too tired to breathe fire." << endl;
+        return;
+    }
+
+    cout << name << " breathes fire: FWOOOOSH!" << endl;
+    fireLevel -= 20;
+    energy -= 15;
+    hunger += 15;
+    happiness += 10;
+
+    if (hunger > 100) hunger = 100;
+    if (happiness > 100) happiness = 100;
+    if (fireLevel < 0) fireLevel = 0;
+}
